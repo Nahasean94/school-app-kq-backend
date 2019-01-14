55c3b265dced5ef8dee148d68ba75c60d09dfea5
Nahashon Njenga <ncubed940@gmail.com>
2019-01-14 09:55:14 +0300

    Added frontend code to make this app launch from one server

---
+++ public/precache-manifest.e97b2a02206f42f619b60aeb82933c90.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "729e7a0231a5d99a8f91",
+    "url": "/static/js/main.729e7a02.chunk.js"
+  },
+  {
+    "revision": "92d048669921af7f62f8",
+    "url": "/static/js/1.92d04866.chunk.js"
+  },
+  {
+    "revision": "ef7ac89ffcc5b64c1d8ef1363bb55bc5",
+    "url": "/index.html"
+  }
+];
